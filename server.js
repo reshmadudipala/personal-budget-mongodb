8bff7b386b34faff14df3cab4e877c4b97bb4de8
reshmadudipala <rdudipal@uncc.edu>
2023-09-24 04:28:11 -0800

    adding cors

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
+//budget api
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/Reshma', express.static('public'));
+//app.use('/Reshma', express.static('public'));
+app.use(cors());
 
-const budget = {mybudget:[
-    {title: 'Eat out' ,
-budget: 30
-},
-{
-    title: 'Rent' ,
-budget: 400
-},
-{
-    title: 'Groceries' ,
-budget: 90
-},
-]};
+const budget = {
+  mybudget: [
+    {
+      title: 'Eat out',
+      budget: 30
+    },
+    {
+      title: 'Rent',
+      budget: 400
+    },
+    {
+      title: 'Groceries',
+      budget: 90
+    },
+  ]
+};
 
-app.get('/',(req, res) => {
-    res.send('Hello World!')
-});
-
-//app.get('/budget01',(req, res) => {
-    //res.json(budget);
+//app.get('/',(req, res) => {
+// res.send('Hello World!')
 //});
+
+app.get('/budget01', (req, res) => {
+  res.json(budget);
+});
 //fetching the json file into server.js file
 
 //fetch('C:\Users\dudip\NBAD\week03\personal-budget1\budget.json')
 //.then(data =>{
-    //console.log(data);
+//console.log(data);
 //});
 //const data = require('./data.json');
 
-const fs = require('fs');
+/*const fs = require('fs');
 
 // Read the JSON file
 fs.readFile('budget.json', 'utf8', (err, data) => {
@@ -55,7 +62,7 @@ fs.readFile('budget.json', 'utf8', (err, data) => {
   }
 });
  
-
-app.listen(port, () =>{
-    console.log('Example app listening at http://localhost:3000');
+*/
+app.listen(port, () => {
+  console.log(' app serving at http://localhost:3000');
 });
